c9d7997e9f5c00b32c3f7fcc69a7b9660ca10166
Eric Papamarcos <ericpapamarcos@users.noreply.github.com>
2018-07-28 20:18:16 -0700

    Update blog home page to remove the full post content in order to better support showing an overview of all posts available

--- blog/theme/index.php
+++ blog/theme/index.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 
+<h1><?php echo get_bloginfo('name'); ?></h1>
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
             <div<?php if (($wp_query->current_post + 1) != $wp_query->post_count) { echo " class=\"story\""; } ?>>
 
-                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-                <p class="caption"><?php echo get_the_date(); ?></p>
-
-                <?php the_content(); ?>
+                <?php the_excerpt(); ?>
 
             </div>
 
